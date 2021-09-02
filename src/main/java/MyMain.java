8ed51112694a938b30bd6f8c2b9dc816ca874060
Rohan <rohanzealous@gmail.com>
2021-09-01 20:06:37 -0700

    I added some more

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -7,7 +7,7 @@ public class MyMain {
         String name = scan.nextLine();
         System.out.println("hello " + name + "\nare you a big baller");
         String answer = scan.nextLine();
-        System.out.println("oh really? I knew it \non a scale of 1-10 how cool are you");
+        System.out.println(answer + "? I knew it\non a scale of 1-10 how cool are you");
         int answer2 = Integer.parseInt(scan.nextLine());
         System.out.println("Hmm I think you are a " + (answer2 - 1) + "\nIs the creator of this chatbot the best person ever?");
         String answer3 = scan.nextLine();
